c0a30f8e2f6994f80fa8228fa7a167e778aacfba
HadsonRamalho <hadsonramalho@gmail.com>
2023-11-09 12:17:13 -0300

    Adicionando funções, organizando comentários
    Funções adicionadas: busca, atribuiChars e exibeConta

--- Projeto_Arabanco/Projeto_Arabanco/Main.cpp
+++ Projeto_Arabanco/Projeto_Arabanco/Main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 /////////////////////////// TO-DO ////////////////////////////////
 /*
-  (Funcao)  |  Tarefa
+         (Funcao)  |  Tarefa
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
   (cadastrarConta) | Verificar se ja existe um cadastro com os dados inseridos
@@ -14,52 +14,53 @@ using namespace std;
 */
 //////////////////////////////////////////////////////////////////
 
+/// 
+/// Constantes
+/// 
+
+const int MAX_CLIENTES = 50;
+int quantidadeDeClientes = 0;
+
+/// 
+/// Structs
+/// 
+
 struct Cliente {
 	char numeroDaConta[12];
 	char numeroDaAgencia[6];
 	char nomeDoTitular[50];
 	char cpfDoTitular[19];
 	float saldoInicial;
+	float saldoAtual;
 };
-
-const int MAX_CLIENTES = 50;
-int quantidadeDeClientes = 0; 
-
 Cliente Clientes[MAX_CLIENTES];
 
+/// 
+/// Protótipos
+/// 
+
+inline void cabecalho_menuPrincipal();
+void menuPrincipal();
+void switch_menuPrincipal(Cliente Clientes[], int opcaoMenu);
+void cadastrarConta();
+int busca(Cliente Clientes[], char campoDeBusca[], int opcaoDeBusca);
+void atribuiChars(Cliente Clientes[], int indice, Cliente Temp);
+void exibeConta(Cliente Clientes[], int indice);
+
+////////////////////////////////////////////////////////////////
+
+int main() {
+	// Configura a localização para pt_BR.utf8
+	setlocale(LC_ALL, "pt_BR");
+	menuPrincipal();
+	return 0;
+}
+
+// Cabeçalho inline
 inline void cabecalho_menuPrincipal() {
 	cout << "**************************************" << endl
 		<< "*   ARABANCO - SEMPRE COM VOCÊ =P    *" << endl
 		<< "**************************************" << endl;
-}
-
-void cadastrarConta() {
-	Cliente Temp; // Tipo temporario, armazena dados para serem verificados antes de serem salvos no sistema
-	cout << " 1 | Cadastrar Conta Corrente" << endl << endl;
-	cout << "Número da Conta: ";
-	cin >> Temp.numeroDaConta;
-	cout << "Número da Agência: ";
-	cin >> Temp.numeroDaAgencia;
-	cout << "Nome do Titular: ";
-	cin >> Temp.nomeDoTitular;
-	cout << "CPF do Titular: ";
-	cin >> Temp.cpfDoTitular;
-	cout << "Saldo inicial: ";
-	cin >> Temp.saldoInicial;
-	while (Temp.saldoInicial < 0) {
-		cerr << "Saldo inválido: O valor não pode ser negativo." << endl << "Digite novamente: ";
-		cin >> Temp.saldoInicial;
-	}
-	Clientes[quantidadeDeClientes] = Temp; // Atribuicao direta (considerar mudar)
-}
-
-void switch_menuPrincipal(int opcaoMenu) {
-	system("CLS");
-	switch (opcaoMenu) {
-	case 1:
-		cadastrarConta();
-		break;
-	}
 }
 
 void menuPrincipal() {
@@ -80,19 +81,114 @@ void menuPrincipal() {
 			<< " 10 | Sair" << endl
 			<< " Escolha uma opção: ";
 		cin >> opcaoMenu;
-		if (opcaoMenu < 1 || opcaoMenu > 10) {
+		if (opcaoMenu < 1 || opcaoMenu > 10) { // Validando a opção escolhida
 			cerr << "Opção inválida. Digite novamente." << endl;
 			system("PAUSE");
 		}
 		else {
-			switch_menuPrincipal(opcaoMenu);
+			switch_menuPrincipal(Clientes, opcaoMenu);
 		}
 	} while (opcaoMenu != 10);
 }
 
-int main() {
-	// Configura a localização para pt_BR.utf8
-	setlocale(LC_ALL, "pt_BR");
-	menuPrincipal();
-	return 0;
+// Acessando funções do programa
+void switch_menuPrincipal(Cliente Clientes[], int opcaoMenu) {
+	system("CLS");
+	switch (opcaoMenu) {
+	case 1:
+		cadastrarConta();
+		break;
+	case 8:
+		for (int i = 0; i < quantidadeDeClientes; i++) {
+			exibeConta(Clientes, i);
+		}
+		system("PAUSE");
+		break;
+	}
+}
+
+// Substituir pela do Jefferson
+void cadastrarConta() {
+	Cliente Temp; // Tipo temporario, armazena dados para serem verificados antes de serem salvos no sistema
+	cout << " 1 | Cadastrar Conta Corrente" << endl << endl;
+	cout << "Número da Conta: ";
+	cin >> Temp.numeroDaConta;
+	while (busca(Clientes, Temp.numeroDaConta, 1) != -1) { //  Se retornar -1, significa que nao existe um cadastro com o campo escolhido
+		cerr << "Já existe uma Conta Corrente com esse número. Digite novamente: ";
+		cin >> Temp.numeroDaConta;
+	}
+	cout << "Número da Agência: ";
+	cin >> Temp.numeroDaAgencia;
+	cout << "Nome do Titular: ";
+	cin >> Temp.nomeDoTitular;
+	cout << "CPF do Titular: ";
+	cin >> Temp.cpfDoTitular;
+	cout << "Saldo inicial: ";
+	cin >> Temp.saldoInicial;
+	while (Temp.saldoInicial < 0) {
+		cerr << "Saldo inválido: O valor não pode ser negativo." << endl << "Digite novamente: ";
+		cin >> Temp.saldoInicial;
+	}
+	atribuiChars(Clientes, quantidadeDeClientes, Temp); // Faz a cópia dos dados inseridos em Temp para Clientes[]
+	quantidadeDeClientes++;
+}
+
+// Usada para verificar se existem cadastros com campos iguais
+
+/// <summary>
+///  Valores para 'opcaoDeBusca': 
+///		1 = Número da Conta
+///		2 = Número da Agência
+///		3 = Nome do Titular
+///		4 = CPF do Titular
+/// </summary>
+
+int busca(Cliente Clientes[], char campoDeBusca[], int opcaoDeBusca) {
+	switch (opcaoDeBusca) {
+	case 1:
+		for (int i = 0; i < quantidadeDeClientes; i++) {
+			if (strcmp(Clientes[i].numeroDaConta, campoDeBusca) == 0) {
+				return i; // Retorna a posição em que foi encontrado um cadastro
+			}
+		}
+		break;
+	case 2:
+		for (int i = 0; i < quantidadeDeClientes; i++) {
+			if (strcmp(Clientes[i].numeroDaAgencia, campoDeBusca) == 0) {
+				return i;
+			}
+		}
+		break;
+	case 3:
+		for (int i = 0; i < quantidadeDeClientes; i++) {
+			if (strcmp(Clientes[i].nomeDoTitular, campoDeBusca) == 0) {
+				return i;
+			}
+		}
+		break;
+	case 4:
+		for (int i = 0; i < quantidadeDeClientes; i++) {
+			if (strcmp(Clientes[i].cpfDoTitular, campoDeBusca) == 0) {
+				return i;
+			}
+		}
+		break;
+	}
+	return -1; // Retorna -1 se não for encontrado um cadastro com os campos buscados
+}
+
+
+// Faz a cópia de campos de Temp para campos de Clientes[indice]
+void atribuiChars(Cliente Clientes[], int indice, Cliente Temp) {
+	strcpy(Clientes[indice].numeroDaConta, Temp.numeroDaConta);
+}
+
+// Exibe todos os campos de uma determinada conta (pode ser usado dentro de um loop)
+void exibeConta(Cliente Clientes[], int indice) {
+	cout << " | Número da Conta: " << Clientes[indice].numeroDaConta << endl
+		<< " | Número da Agência: " << Clientes[indice].numeroDaAgencia << endl
+		<< " | Nome do Titular: " << Clientes[indice].nomeDoTitular << endl
+		<< " | CPF do Titular: " << Clientes[indice].cpfDoTitular << endl
+		<< " | Saldo Atual: " << Clientes[indice].saldoAtual << endl
+		<< "---------------------------------" << endl;
 }
