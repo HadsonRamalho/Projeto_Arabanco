99099395ecd868f8d33d4177ace79a6853e49179
Hadson <hadsonramalho@gmail.com>
2023-11-10 15:31:01 -0300

    adicionando a função alterarConta

--- Projeto_Arabanco/Projeto_Arabanco/Main.cpp
+++ Projeto_Arabanco/Projeto_Arabanco/Main.cpp
@@ -46,7 +46,7 @@ void cadastrarConta();
 int busca(Cliente Clientes[], char campoDeBusca[], int opcaoDeBusca);
 void atribuiChar(Cliente Clientes[], int indice, Cliente Temp);
 void exibeConta(Cliente Clientes[], int indice);
-void altera();
+void alterarConta(Cliente Clientes[]);
 
 ////////////////////////////////////////////////////////////////
 
@@ -99,6 +99,11 @@ void switch_menuPrincipal(Cliente Clientes[], int opcaoMenu) {
 	case 1:
 		cadastrarConta();
 		break;
+
+	case 2:
+		alterarConta(Clientes);
+		system("PAUSE");
+		break;
 	case 8:
 		for (int i = 0; i < quantidadeDeClientes; i++) {
 			exibeConta(Clientes, i);
@@ -130,6 +135,7 @@ void cadastrarConta() {
 		cerr << "Saldo inválido: O valor não pode ser negativo." << endl << "Digite novamente: ";
 		cin >> Temp.saldoInicial;
 	}
+	Temp.saldoAtual = Temp.saldoInicial;
 	atribuiChar(Clientes, quantidadeDeClientes, Temp); // Faz a cópia dos dados inseridos em Temp para Clientes[]
 	quantidadeDeClientes++;
 }
@@ -182,6 +188,11 @@ int busca(Cliente Clientes[], char campoDeBusca[], int opcaoDeBusca) {
 // Faz a cópia de campos de Temp para campos de Clientes[indice]
 void atribuiChar(Cliente Clientes[], int indice, Cliente Temp) {
 	strcpy(Clientes[indice].numeroDaConta, Temp.numeroDaConta);
+	strcpy(Clientes[indice].numeroDaAgencia, Temp.numeroDaAgencia);
+	strcpy(Clientes[indice].nomeDoTitular, Temp.nomeDoTitular);
+	strcpy(Clientes[indice].cpfDoTitular, Temp.cpfDoTitular);
+	Clientes[indice].saldoInicial = Temp.saldoInicial;
+	Clientes[indice].saldoAtual = Temp.saldoAtual;
 }
 
 // Exibe todos os campos de uma determinada conta (pode ser usado dentro de um loop)
@@ -192,4 +203,49 @@ void exibeConta(Cliente Clientes[], int indice) {
 		<< " | CPF do Titular: " << Clientes[indice].cpfDoTitular << endl
 		<< " | Saldo Atual: " << Clientes[indice].saldoAtual << endl
 		<< "---------------------------------" << endl;
+}
+
+void alterarConta(Cliente Clientes[]) {
+	char numConta[12], numAgencia[6]; // Os campos que serão buscados
+	int resultadoDaBusca; // Armazena o resultado da função de busca
+	bool validacao1 = false, validacao2 = false; // Faz a validação de cada um dos campos buscados
+	int indiceResultado1 = -1, indiceResultado2 = -2; // Armazena o índice em que foi encontrado uma conta com o campo buscado
+
+	cout << " | Insira o Número da Conta Corrente: ";
+	cin >> numConta;
+	cout << " | Insira o Número da Agência da Conta Corrente: ";
+	cin >> numAgencia;
+
+	resultadoDaBusca = busca(Clientes, numConta, 1); // Verifica se há uma conta com o número procurado, e armazena o resultado
+	if (resultadoDaBusca != -1) {
+		validacao1 = true; // Valida a busca pelo número da conta
+		indiceResultado1 = resultadoDaBusca; // Armazena o índice em que foi encontrado a conta
+	}
+
+	resultadoDaBusca = busca(Clientes, numAgencia, 2); // Faz o mesmo que o de cima, mas buscando o número da agência
+	if (resultadoDaBusca != -1) {
+		validacao2 = true;
+		indiceResultado2 = resultadoDaBusca;
+	}
+
+	if (!validacao1 || !validacao2) { // Se alguma das validações falhar, exibe um erro na tela e retorna ao menu principal
+		cerr << " Conta não encontrada! " << endl;
+		return;
+	}
+
+	if (validacao1 && validacao2 && indiceResultado1 == indiceResultado2) { // Se todas as validações passarem, e os índices das contas forem os mesmos, segue com a alteração dos dados
+		cout << " -  Conta encontrada  -" << endl
+			<< " | Titular Atual: " << Clientes[indiceResultado1].nomeDoTitular << endl
+			<< " | CPF do Titular: " << Clientes[indiceResultado1].cpfDoTitular << endl << endl;
+
+		cout << " | Insira o novo Nome do Titular: ";
+		cin >> Clientes[indiceResultado1].nomeDoTitular;
+		cout << " | Insira o novo CPF do Titular: ";
+		cin >> Clientes[indiceResultado1].cpfDoTitular;
+		cout << endl << " - Dados da Conta - " << endl
+			<< " | Número da Conta Corrente: " << Clientes[indiceResultado1].numeroDaConta << endl
+			<< " | Número da Agência: " << Clientes[indiceResultado1].numeroDaAgencia << endl
+			<< " | [NOVO] Nome do Titular: " << Clientes[indiceResultado1].nomeDoTitular << endl
+			<< " | [NOVO] CPF do Titular: " << Clientes[indiceResultado1].cpfDoTitular << endl;
+	}
 }
